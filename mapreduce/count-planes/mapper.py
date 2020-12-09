5b66b43e9e8dbb0a04d96dba2ded40cfdfd53f77
Emma Rizzi <rizziemma22@gmail.com>
2020-12-09 17:34:49 +0100

    update mapper

--- mapreduce/count-planes/mapper.py
+++ mapreduce/count-planes/mapper.py
@@ -6,14 +6,7 @@ try:
 	import asterix
 	import dpkt
 except ImportError:
-	#zipdir = "env-mapreduce.zip"
 	libdir = 'env-mapreduce.zip/lib/python2.7/site-packages'
-
-	#zipdir_exists = os.path.exists(zipdir)
-	#if zipdir_exists :
-	#	import zipfile
-	#	with zipfile.ZipFile(zipdir, 'r') as zip_ref:
-	#		zip_ref.extractall(libdir)
 
 	if os.path.exists(libdir):
 		if os.path.isdir(libdir) and libdir not in sys.path:
@@ -25,48 +18,50 @@ except ImportError:
 	import asterix
         import dpkt
 
-	read_input = False
-	infile = sys.stdin
+read_input = False
+infile = sys.stdin
 
-	while(not read_input and not(infile is  None)) :
-		try :
-			pcap = dpkt.pcap.Reader(infile)
-			read_input = True
-		except Exception:
-			if infile.readline() is None :
-				infile = None
-
-	cntr=1
+for i in range(100):
 	if read_input :
-		try :
-			for ts, buf in pcap:
-				try :
-					eth = dpkt.ethernet.Ethernet(buf)
-					data = eth.ip.udp.data
-					cntr += 1
-					parsed = asterix.parse(data)
-					time = False
+		break
+	try :
+		pcap = dpkt.pcap.Reader(infile)
+		read_input = True
+	except Exception :
+		infile.readline()
+cntr=1
+if read_input :
+	try :
+		for ts, buf in pcap:
+			try :
+				eth = dpkt.ethernet.Ethernet(buf)
+				data = eth.ip.udp.data
+				cntr += 1
+				parsed = asterix.parse(data)
+				time = False
 
-					for packet in parsed :
-						plane = False
+				for packet in parsed :
+					plane = False
 
-						if 'I220' in packet :
-							if 'ACAddr' in packet['I220']:
-								if 'val' in packet['I220']['ACAddr']:
-									plane = str(packet['I220']['ACAddr']['val'])
+					if 'I220' in packet :
+						if 'ACAddr' in packet['I220']:
+							if 'val' in packet['I220']['ACAddr']:
+								plane = str(packet['I220']['ACAddr']['val'])
 
-						for time_label in ['I030', 'I140'] :
-							if time_label in packet :
-								if 'ToD' in packet[time_label] :
-									if 'val' in packet[time_label]['ToD'] :
-										time = float(packet[time_label]['ToD']['val'])
-										time = int(time / 3600) #seconds to hours
-										break
+					for time_label in ['I030', 'I140'] :
+						if time_label in packet :
+							if 'ToD' in packet[time_label] :
+								if 'val' in packet[time_label]['ToD'] :
+									time = float(packet[time_label]['ToD']['val'])
+									time = int(time / 3600) #seconds to hours
+									break
 
-						if time and plane :
-							print('{0}\t{1}'.format(time, plane))
-				except Exception as e :
-					#print(e)
-					continue
-		except Exception as e :
-			pass
+					if time and plane :
+						print('{0}\t{1}'.format(time, plane))
+			except Exception as e :
+				#print(e)
+				continue
+	except Exception as e :
+		pass
+else :
+	infile.read()
