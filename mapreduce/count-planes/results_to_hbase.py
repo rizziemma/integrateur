0822d0a1c36f1dbda8807ab6495c210113e8a5af
rizziemma <rizziemma22@gmail.com>
2020-12-27 19:59:13 +0100

    update scripts analyse

--- mapreduce/count-planes/results_to_hbase.py
+++ mapreduce/count-planes/results_to_hbase.py
@@ -1,6 +1,25 @@
-import hbase
+import happybase
 import sys
+import csv
 
+conn = happybase.Connection(host='192.168.37.40', port=50001)
 
-with open(sys.argv[1]) as f :
-	#upload
+table = conn.table('data-plane')
+b = table.batch()
+print("connected to database")
+
+name = sys.argv[1] + '-'
+i = 0
+
+with open(sys.argv[2]) as f :
+	reader = csv.reader(f, delimiter='\t')
+	for r in reader :
+		if(i%100 == 0):
+			print("push line " + str(i))
+		#upload
+		key = name + str(i)
+		b.put(key, {b'date:date':r[0], b'hour:hour':r[1], b'manufacturer:manufacturer':r[2], b'operator:operator':r[3], b'model:model':r[4], b'planes:planes':r[5]})
+		i = i + 1
+
+print("sending batch")
+b.send()
--- mapreduce/count-planes/run_analysis.sh
+++ mapreduce/count-planes/run_analysis.sh
@@ -12,11 +12,11 @@ echo "extracting files"
 lzip -d /data2/$1/*
 
 #making csv files
-rm -rf /data2/out
-mkdir /data2/out
+rm -rf /data2/out$1
+mkdir /data2/out$1
 
-echo "map to csv"
-python map_to_csv.py /data2/$1/ /data2/out/ $2
+#echo "map to csv"
+python map_to_csv.py /data2/$1/ /data2/out$1/ $2
 
 #cleaning up unzip files
 echo "deleting extracted files"
@@ -24,16 +24,22 @@ rm -rf /data2/$1
 
 #hadoop
 echo "upload to hdfs"
-hadoop fs -rm -r -skipTrash in
-hadoop fs -mkdir in
-hadoop fs -put /data2/out/* in
+hadoop fs -rm -r -skipTrash in$1
+hadoop fs -mkdir in$1
+hadoop fs -put /data2/out$1/* in$1
 
 echo "running mapreduce"
-mapreduce $3 $4 in out
+mapreduce $3 $4 in$1 out$1
 
 #uploading results to hbase
 echo "downloading results"
-rm /data2/results
-hadoop fs -get out/part-00000 /data2/results
-python results_to_hbase.py /data2/results
+rm /data2/results$1
+hadoop fs -get out$1/part-00000 /data2/results$1
+python results_to_hbase.py $1 /data2/results$1
 
+hadoop fs -rm -r -skipTrash in$1
+hadoop fs -rm -r -skipTrash out$1
+rm -rf /data2/out$1/
+
+#print end time
+date
