283a969bbb3b788e8f7bee6d19562d2d106b937d
rizziemma <rizziemma22@gmail.com>
2021-01-10 12:42:43 +0100

    try catch on map csv

--- mapreduce/count-planes/map_to_csv.py
+++ mapreduce/count-planes/map_to_csv.py
@@ -24,46 +24,48 @@ with open(database_plane) as f :
 print("starting file to csv")
 for filename in os.listdir(input_dir) :
 	print("to csv : " + filename)
+	try :
+		name = filename.split('.pcap')[0]
+		date = name[0:10]
 
-	name = filename.split('.pcap')[0]
-	date = name[0:10]
+		with open(input_dir+filename) as in_file :
+			with open(output_dir+name+'.csv', 'w') as out_file :
+				writer = csv.writer(out_file, delimiter='\t')
+				pcap = dpkt.pcap.Reader(in_file)
 
-	with open(input_dir+filename) as in_file :
-		with open(output_dir+name+'.csv', 'w') as out_file :
-			writer = csv.writer(out_file, delimiter='\t')
-			pcap = dpkt.pcap.Reader(in_file)
+				cntr=1
+				for ts, buf in pcap:
+					eth = dpkt.ethernet.Ethernet(buf)
+					data = eth.ip.udp.data
 
-			cntr=1
-			for ts, buf in pcap:
-				eth = dpkt.ethernet.Ethernet(buf)
-				data = eth.ip.udp.data
+					cntr += 1
 
-				cntr += 1
+					# Parse data
+					parsed = asterix.parse(data)
 
-				# Parse data
-				parsed = asterix.parse(data)
+					time = False
 
-				time = False
+					for packet in parsed :
+						plane = [False for i in range(6)]
+						addr = False
+						if 'I220' in packet :
+							if 'ACAddr' in packet['I220']:
+								if 'val' in packet['I220']['ACAddr']:
+									addr = str(packet['I220']['ACAddr']['val'])
 
-				for packet in parsed :
-					plane = [False for i in range(6)]
-					addr = False
-					if 'I220' in packet :
-						if 'ACAddr' in packet['I220']:
-							if 'val' in packet['I220']['ACAddr']:
-								addr = str(packet['I220']['ACAddr']['val'])
-
-					for time_label in ['I030', 'I140'] :
-						if time_label in packet :
-							if 'ToD' in packet[time_label] :
-								if 'val' in packet[time_label]['ToD'] :
-									time = float(packet[time_label]['ToD']['val'])
-									time = int(time / 3600) #seconds to hours
-									break
-					if date and addr :
-						if addr in planes :
-							line = planes[addr]
-							plane = [date, time, line['manufacturer'], line['operator'], line['model'], addr]
-						else :
-							plane = [date, time, False, False, False, addr]
-						writer.writerow(plane)
+						for time_label in ['I030', 'I140'] :
+							if time_label in packet :
+								if 'ToD' in packet[time_label] :
+									if 'val' in packet[time_label]['ToD'] :
+										time = float(packet[time_label]['ToD']['val'])
+										time = int(time / 3600) #seconds to hours
+										break
+						if date and addr :
+							if addr in planes :
+								line = planes[addr]
+								plane = [date, time, line['manufacturer'], line['operator'], line['model'], addr]
+							else :
+								plane = [date, time, False, False, False, addr]
+							writer.writerow(plane)
+	except Exception as a :
+		continue
